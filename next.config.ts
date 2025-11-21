af8d5d7e1edf88974459e25c8524b7cea805d948
kumaraswini-11 <kumaraswini.11.11@gmail.com>
2025-11-21 20:43:39 +0530

    feat: update TinyLink setup and cleanup project assets
    - update README and layout
    - update ESLint & next.config configuration
    - remove unused SVG files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  typedRoutes: true,
+  reactCompiler: true,
+  cacheComponents: true,
 };
 
 export default nextConfig;
